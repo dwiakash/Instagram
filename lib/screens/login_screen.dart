013e6281efd7d26d43a16884b00254dc6a491f9c
Akash Dwivedi <dwiakash.2003@gmail.com>
2024-03-09 20:29:33 +0530

    create login page

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:instagram/utils/colors.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -14,16 +16,20 @@ class _LoginScreenState extends State<LoginScreen> {
         child: Container(
       padding: const EdgeInsets.symmetric(horizontal: 32),
       width: double.infinity,
-      child: const Column(
+      child: Column(
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
           //svg image
-          
+          // ignore: deprecated_member_use
+          SvgPicture.asset(
+            'assets/ic_instagram.svg',
+            // color: primaryColor ,
+            height: 64,
+          )
           //tetx field input form email
           //text field input for password
           //button for login
           //transitiong to signup
-
         ],
       ),
     ));
